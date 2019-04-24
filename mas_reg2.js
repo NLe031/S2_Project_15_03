a629e028c96055fbd3fdd651bacf1f4b0abfa4c3
Nicholas Le <nle031@west-mec.org>
2019-04-24 16:25:03 -0700

    Final Commit
    copy and pasted the code from the other js file and added the event listener to write session values.
    Ready for Review.

--- mas_reg2.js
+++ mas_reg2.js
@@ -20,3 +20,18 @@
 
 
 */
+//Prints the values onto the payment page
+function writeSessionValues() {
+      document.getElementById("regName").textContent = sessionStorage.confName;
+      document.getElementById("regGroup").textContent = sessionStorage.confGroup;
+      document.getElementById("regEmail").textContent = sessionStorage.confMail;
+      document.getElementById("regPhone").textContent = sessionStorage.confPhone;
+      document.getElementById("regSession").textContent = sessionStorage.confSession;
+      document.getElementById("regBanquet").textContent = sessionStorage.confBanquet;
+      document.getElementById("regPack").textContent = sessionStorage.confPack;
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.confTotal;
+}
+//used to write stored info onto the payment page
+window.addEventListener("load", function () {
+      writeSessionValues();
+})
